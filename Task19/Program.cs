1be63f3587c20a363ab3c991e4aeaee73cdb5b22
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-29 18:25:17 +0300

    Cykle fail

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -22,73 +22,36 @@ int QuantInt(int number)
 
 int quantity=QuantInt(num);
 
-//Console.WriteLine(QuantInt(num));
-// int quantity1=quantity;
-// int quantity2=quantity;
-// int quantity3=quantity;
 
-// int first=num;
-// int second=num;
-
-    
-//     while (quantity-1>0)
-//     {
-//     first=first/10;
-//     quantity--;
-//     }
-// Console.WriteLine($"{first}");
-
-//    if (first==num%10)
-//    {
-//     while (quantity1-2>0)
-//     {
-//     second=second/10;
-//     quantity1--;
-//     }
-// second=second%10;
-// Console.WriteLine($"{second}");
-//    }
-//    else 
-//    {
-//     Console.WriteLine("Не полиндром");
-//    return;
-//    }
-// Console.WriteLine($"{num%10}");
-//Console.WriteLine($"{quantity1}");
-// int simetry=quantity2/2;
-
-// Console.WriteLine($"{simetry}");
 int [] arr=new int[quantity];
-// int i=0;
 int krat=1;
-// while (i<quantity)
+int rightSide=quantity;
+
 for (int i = 0; i < quantity; i++)
 {
-    arr[i]=(num/(10*krat))%10;
+    arr[i]=(num/(10*krat))%10; // 12345/100=123 мб добавить заполнение для первой позиции. Помни, что цикл перевернут! С этим разобраться
     krat=krat*10;
-    Console.Write($"{arr[i]}  ");
+    Console.Write($"{arr[i]}");
 }
-arr[quantity-1]=num%10;
-int simetry=quantity/2;
-int steps=1;
-int count=1;
-while (steps!=simetry)
-{
-    int smth=arr[quantity-quantity]==arr[quantity-count]?steps++:steps--;
-    quantity++;
-    count++;
-}
-if (steps<simetry) Console.WriteLine("Не полиндром");
-else Console.WriteLine("Таки полиндром!!!");
-// else Console.WriteLine("Не полиндром");
+// arr[quantity-1]=(num/10)%10;
+// arr[quantity-rightSide]=num%10;
 
+// int simetry=quantity/2;
+// int steps=0;
 // int count=1;
-// int rightSight=quantity;
-// while (quantity!=quantity)
+
+// while (arr[quantity-rightSide]==arr[quantity-count]&&count-1!=simetry)
 // {
-//    int pairs= arr[quantity-rightSight]==arr[quantity-count]?1 :-1;
-//    count++;
-//    rightSight++;
+//     rightSide--;
+//     count++;
+//     steps++;
+// }
+// Console.WriteLine($">{arr[4]}");
+// Console.WriteLine($"steps>{steps}");
+
+// if (steps<simetry) Console.WriteLine("Не полиндром");
+// else Console.WriteLine("Таки полиндром!!!");
 
 
+//Console.WriteLine($"{}");
 
