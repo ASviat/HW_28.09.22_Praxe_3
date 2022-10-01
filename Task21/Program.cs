ef1f1ad2884b366d9b455d7a5692c5ad719c5682
Sviatoslav <ecrprague.asi@gmail.com>
2022-10-01 09:17:03 +0300

    Task 21 Done

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -3,3 +3,36 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+
+Console.WriteLine("Введите координаты точки 1, на оси Х: ");
+int x1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки 1, на оси Y: ");
+int y1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки 1, на оси Z: ");
+int z1=Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine("Введите координаты точки 2, на оси Х: ");
+int x2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки 2, на оси Y: ");
+int y2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки 2, на оси Z: ");
+int z2=Convert.ToInt32(Console.ReadLine());
+
+
+
+
+double Distance(int x1, int y1, int z1, int x2, int y2, int z2) 
+{
+    double n1=Math.Pow(x2-x1, 2);
+    double n2=Math.Pow(y2-y1, 2);
+    double n3=Math.Pow(z2-z1, 2);
+    double sum=Math.Sqrt(n1+n2+n3);
+    return Math.Round(sum,2,MidpointRounding.ToZero);
+}
+
+double distance=Distance(x1,y1,z1,x2,y2,z2);
+Console.WriteLine($"{distance}");
