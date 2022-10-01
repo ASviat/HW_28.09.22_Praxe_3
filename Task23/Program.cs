7f0042bb8acc10fa5d6cb020cb362cb20a6ae820
Sviatoslav <ecrprague.asi@gmail.com>
2022-10-01 09:55:12 +0300

    Task 23 Done

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -4,3 +4,14 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число, дабы узреть его куб: ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+int first=1;
+while(first<=num)
+{
+    int powed=first*first*first;
+    Console.Write($"{powed}  ");
+    first++;
+}
